9bad7da404e5e740597e335e8eeb8f5af259db55
ruzo-814 <unagi.0418.water@gmail.com>
2025-09-11 13:17:28 +0900

    第10回演習課題

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -1,10 +1,10 @@
 package raisetech.Student.Management;
 
-import java.util.Map;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-  private String name = "LAO Student";
-  private String age = "28";
-
-
-  private Map<String, String> student;
+  @Autowired
+  private StudentRepository repository;
 
 
   public static void main(String[] args) {
@@ -24,23 +21,13 @@ public class Application {
     SpringApplication.run(Application.class, args);
   }
 
-  @GetMapping("/studentInfo")
-  public String getStudentInfo() {
-
-    return this.name + " " + this.age + "age";
+  @GetMapping("/students")
+  public List<Student> getStudentList () {
+    return repository.searchStudent();
   }
 
-
-  @PostMapping("/studentInfo")
-  public void setStudentInfo(String name, String age) {
-    this.name = student.get("name");
-    this.age = student.get("age");
-  }
-
-  @PostMapping("/studentName")
-  public void updateStudentName(String name) {
-    this.name = name;
+  @GetMapping("/studentCourses")
+  public List<StudentCourses> getStudentCousesList(){
+    return repository.searchStudentCourses();
   }
 }
-
-
