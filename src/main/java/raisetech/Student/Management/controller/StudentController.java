6f6fb71799faa50e03d3c99364bedcdd1c87758e
ruzo-814 <unagi.0418.water@gmail.com>
2025-09-18 12:08:54 +0900

    第14回演習課題

--- src/main/java/raisetech/Student/Management/controller/StudentController.java
+++ src/main/java/raisetech/Student/Management/controller/StudentController.java
@@ -1,19 +1,17 @@
 package raisetech.Student.Management.controller;
 
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.Student.Management.controller.converter.StudentConverter;
 import raisetech.Student.Management.data.Student;
 import raisetech.Student.Management.data.StudentCourses;
-import raisetech.Student.Management.domain.StudentDetail;
 import raisetech.Student.Management.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private StudentService service;
@@ -26,11 +24,12 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentCourses> studentCourses = service.searchStudentCousesList();
 
-    return converter.convertStudentDetails(students, studentCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses)); //←studentDetailから返ってきた生徒情報を入れるリスト名
+    return "studentList"; //←HTMLのクラス名（上のstudentListとは別物。上のは名前任意）
   }
 
   @GetMapping("/studentCoursesList")
