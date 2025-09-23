c5a97ee953e8b9a059c6af603e6430a7319f27c4
ruzo-814 <unagi.0418.water@gmail.com>
2025-09-23 12:07:55 +0900

    第14回演習課題

--- src/main/java/raisetech/Student/Management/controller/StudentController.java
+++ src/main/java/raisetech/Student/Management/controller/StudentController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.Student.Management.controller.converter.StudentConverter;
 import raisetech.Student.Management.data.Student;
 import raisetech.Student.Management.data.StudentCourses;
+import raisetech.Student.Management.domain.StudentDetail;
 import raisetech.Student.Management.service.StudentService;
 
 @Controller
@@ -28,7 +31,8 @@ public class StudentController {
     List<Student> students = service.searchStudentList();
     List<StudentCourses> studentCourses = service.searchStudentCousesList();
 
-    model.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses)); //←studentDetailから返ってきた生徒情報を入れるリスト名
+    model.addAttribute("studentList", converter.convertStudentDetails(students,
+        studentCourses)); //←studentDetailから返ってきた生徒情報を入れるリスト名
     return "studentList"; //←HTMLのクラス名（上のstudentListとは別物。上のは名前任意）
   }
 
@@ -36,4 +40,19 @@ public class StudentController {
   public List<StudentCourses> getStudentCousesList() {
     return service.searchStudentCousesList();
   }
+
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "registerStudent";
+  }
+
+
+  @PostMapping("/registerStudent")
+  //↓studentはregisterStudentの${student}の部分
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail) {
+    return "";
+  }
+
 }
