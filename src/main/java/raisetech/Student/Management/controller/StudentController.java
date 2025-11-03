cf6aba6b2966b50620ac9f4c37baca6aca2028d2
ruzo-814 <unagi.0418.water@gmail.com>
2025-11-03 13:36:28 +0900

    resolve conflict: keep kadai21 version

--- src/main/java/raisetech/Student/Management/controller/StudentController.java
+++ src/main/java/raisetech/Student/Management/controller/StudentController.java
@@ -1,24 +1,16 @@
 package raisetech.Student.Management.controller;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/main
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PathVariable;
-=======
->>>>>>> origin/main
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.Student.Management.data.StudentCourses;
 import raisetech.Student.Management.domain.StudentDetail;
 import raisetech.Student.Management.service.StudentService;
-
 
 /**
  * 受講生の検索や登録、更新などを行うREST APIとして実行されるControllerです。
@@ -33,7 +25,6 @@ public class StudentController {
     this.service = service;
   }
 
-
   /**
    * 受講生一覧検索です。
    * 全件検索を行うので、条件指定は行いません。
@@ -44,7 +35,6 @@ public class StudentController {
   public List<StudentDetail> getStudentList() {
     return service.searchStudentList();
   }
-
 
   /**
    * 受講生検索です。
@@ -58,7 +48,6 @@ public class StudentController {
     return service.searchStudent(id);
   }
 
-
   /**
    * 受講コース情報検索です。
    * 全件検索を行うので、条件指定は行いません。
@@ -70,8 +59,6 @@ public class StudentController {
     return service.searchStudentCousesList();
   }
 
-<<<<<<< HEAD
-
   /**
    * 新規受講生登録です。
    *
@@ -82,14 +69,7 @@ public class StudentController {
   public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail) {
     StudentDetail responceStudentDetail = service.registerStudent(studentDetail);
     return ResponseEntity.ok(responceStudentDetail);
-=======
-  @PostMapping("/registerStudent")
-  public ResponseEntity<String> registerStudent(@RequestBody StudentDetail studentDetail) {
-    service.registerStudent(studentDetail);
-    return ResponseEntity.ok("登録処理が成功しました。");
->>>>>>> origin/main
   }
-
 
   /**
    * 受講生情報更新です。
