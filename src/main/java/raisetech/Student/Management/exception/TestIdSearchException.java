217b1797f93fdf5771e42ec24030d869aa68a3f3
ruzo-814 <unagi.0418.water@gmail.com>
2025-11-08 00:55:37 +0900

    第23回演習課題

---
+++ src/main/java/raisetech/Student/Management/exception/TestIdSearchException.java
@@ -0,0 +1,20 @@
+package raisetech.student.management.exception;
+
+public class TestIdSearchException extends Exception {
+
+  public TestIdSearchException() {
+    super();
+  }
+
+  public TestIdSearchException(String message) {
+    super(message);
+  }
+
+  public TestIdSearchException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public TestIdSearchException(Throwable cause) {
+    super(cause);
+  }
+}
