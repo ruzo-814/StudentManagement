cad81b80750dc767bae98d28df065f90362ab8c4
ruzo-814 <unagi.0418.water@gmail.com>
2025-11-07 23:27:11 +0900

    第22回演習課題

--- src/main/java/raisetech/Student/Management/controller/StudentController.java
+++ src/main/java/raisetech/Student/Management/controller/StudentController.java
@@ -1,12 +1,13 @@
 package raisetech.student.management.controller;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management.data.StudentCourse;
 import raisetech.student.management.domain.StudentDetail;
+import raisetech.student.management.exception.TestException;
 import raisetech.student.management.service.StudentService;
 
 /**
@@ -32,31 +34,29 @@ public class StudentController {
   }
 
   /**
-   * 受講生一覧検索です。
-   * 全件検索を行うので、条件指定は行いません。
+   * 受講生一覧検索です。 全件検索を行うので、条件指定は行いません。
    *
    * @return 受講生一覧（全件）
    */
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
-    return service.searchStudentList();
+  public List<StudentDetail> getStudentList() throws TestException {
+    throw new TestException(
+        "現在このAPIは利用できません。URLは「studentList」ではなく「students」を利用してください。");
   }
 
   /**
-   * 受講生詳細検索です。
-   * IDに紐づく任意の受講生の情報を取得します。
+   * 受講生詳細検索です。 IDに紐づく任意の受講生の情報を取得します。
    *
    * @param id 受講生ID
    * @return 受講生
    */
   @GetMapping("/student/{id}")
-  public StudentDetail getStudent(@PathVariable @Size(min=1, max=3) String id) {
+  public StudentDetail getStudent(@PathVariable @Size(min = 1, max = 3) String id) {
     return service.searchStudent(id);
   }
 
   /**
-   * 受講コース情報検索です。
-   * 全件検索を行うので、条件指定は行いません。
+   * 受講コース情報検索です。 全件検索を行うので、条件指定は行いません。
    *
    * @return 受講コース情報一覧（全件）
    */
@@ -72,14 +72,14 @@ public class StudentController {
    * @return 実行結果
    */
   @PostMapping("/registerStudent")
-  public ResponseEntity<StudentDetail> registerStudent(@RequestBody @Valid StudentDetail studentDetail) {
+  public ResponseEntity<StudentDetail> registerStudent(
+      @RequestBody @Valid StudentDetail studentDetail) {
     StudentDetail responceStudentDetail = service.registerStudent(studentDetail);
     return ResponseEntity.ok(responceStudentDetail);
   }
 
   /**
-   * 受講生情報更新です。
-   * キャンセルフラグの更新もここで行います。（論理削除）
+   * 受講生情報更新です。 キャンセルフラグの更新もここで行います。（論理削除）
    *
    * @param studentDetail 受講生詳細情報
    * @return 実行結果
