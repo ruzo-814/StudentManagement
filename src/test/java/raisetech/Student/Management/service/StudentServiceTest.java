7f6509e2b36fc0b602a83915b0afa28d7d9c1364
ruzo-814 <unagi.0418.water@gmail.com>
2025-11-14 15:03:20 +0900

    第27回演習課題

---
+++ src/test/java/raisetech/Student/Management/service/StudentServiceTest.java
@@ -0,0 +1,53 @@
+package raisetech.Student.Management.service;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import raisetech.student.management.controller.converter.StudentConverter;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentCourse;
+import raisetech.student.management.domain.StudentDetail;
+import raisetech.student.management.repository.StudentRepository;
+import raisetech.student.management.service.StudentService;
+
+@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+
+  @Mock
+  private StudentRepository repository;
+
+  @Mock
+  private StudentConverter converter;
+
+  @Test
+  void 受講生詳細の一覧検索_リポジトリとコンバーターの処理が適切に呼び出せていること() {
+    //事前準備
+    StudentService sut = new StudentService(repository, converter);
+    List<Student> studentList = new ArrayList<>();
+    List<StudentCourse> studentCourseList = new ArrayList<>();
+
+    when(repository.search()).thenReturn(studentList);
+    when(repository.searchStudentCourseList()).thenReturn(studentCourseList);
+
+    //実行
+//    List<StudentDetail> actual = sut.searchStudentList();
+   sut.searchStudentList();
+
+    //検証
+    verify(repository, times(1)).search();
+    verify(repository, times(1)).searchStudentCourseList();
+    verify(converter, times(1)).convertStudentDetails(studentList, studentCourseList);
+
+    //後処理
+    //例）ここでDBを元に戻る
+  }
+}
