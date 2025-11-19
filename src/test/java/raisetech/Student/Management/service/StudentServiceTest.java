61e5741142e336e8a7a735aeb8b71c4a74b186ee
ruzo-814 <unagi.0418.water@gmail.com>
2025-11-19 10:27:44 +0900

    第27回演習課題

--- src/test/java/raisetech/Student/Management/service/StudentServiceTest.java
+++ src/test/java/raisetech/Student/Management/service/StudentServiceTest.java
@@ -1,5 +1,6 @@
 package raisetech.student.management.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -10,14 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
 import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.repository.StudentRepository;
-import raisetech.student.management.service.StudentService;
+
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
@@ -32,14 +32,13 @@ class StudentServiceTest {
   private StudentService sut;
 
   @BeforeEach
-  void before(){
+  void before() {
     sut = new StudentService(repository, converter);
   }
 
   @Test
   void 受講生詳細の一覧検索_リポジトリとコンバーターの処理が適切に呼び出せていること() {
     //事前準備
-    StudentService sut = new StudentService(repository, converter);
     List<Student> studentList = new ArrayList<>();
     List<StudentCourse> studentCourseList = new ArrayList<>();
 
@@ -47,7 +46,7 @@ class StudentServiceTest {
     when(repository.searchStudentCourseList()).thenReturn(studentCourseList);
 
     //実行
-   sut.searchStudentList();
+    sut.searchStudentList();
 
     //検証
     verify(repository, times(1)).search();
@@ -57,4 +56,69 @@ class StudentServiceTest {
     //後処理
     //例）ここでDBを元に戻る
   }
+
+
+  @Test
+  void 受講生詳細のID検索_リポジトリとコンバーターの処理が適切に呼び出せていること() {
+    //事前準備
+    String id = "14";
+    Student student = new Student();
+    student.setId(id);
+    List<StudentCourse> studentCourseList = new ArrayList<>();
+
+    when(repository.searchStudent(id)).thenReturn(student);
+    when(repository.searchStudentCourse(id)).thenReturn(studentCourseList);
+
+    StudentDetail expected = new StudentDetail(student, studentCourseList);//expected = 期待する結果
+
+    //実行
+    StudentDetail actual = sut.searchStudent(id);//actual = 実際の結果
+
+    //検証
+    verify(repository, times(1)).searchStudent(id);
+    verify(repository, times(1)).searchStudentCourse(id);
+
+    assertEquals(expected.getStudent().getId(), actual.getStudent().getId());
+
+    //後処理
+  }
+
+
+  @Test
+  void 新規受講生登録_リポジトリの処理が適切に呼び出せていること() {
+    //事前準備
+    Student student = new Student();
+    StudentCourse studentCourse = new StudentCourse();
+    List<StudentCourse> studentCourseList = List.of(studentCourse);
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+
+    //実行
+    sut.registerStudent(studentDetail);
+
+    //検証
+    verify(repository, times(1)).insertStudent(student);
+    verify(repository, times(1)).insertStudentCourse(studentCourse);
+
+    //後処理
+  }
+
+
+  @Test
+  void 受講生情報の更新_リポジトリの処理が適切に呼び出せていること() {
+    //事前準備
+    Student student = new Student();
+    StudentCourse studentCourse = new StudentCourse();
+    List<StudentCourse> studentCourseList = List.of(studentCourse);
+    StudentDetail studentDetail = new StudentDetail(student, studentCourseList);
+
+    //実行
+    sut.updateStudent(studentDetail);
+
+    //検証
+    verify(repository, times(1)).updateStudent(student);
+    verify(repository, times(1)).updateStudentCourse(studentCourse);
+
+    //後処理
+  }
+
 }
