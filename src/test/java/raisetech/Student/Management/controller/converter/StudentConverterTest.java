36dfaf8b47bf3d323b4404271745088548d15998
ruzo-814 <unagi.0418.water@gmail.com>
2025-11-28 14:04:27 +0900

    第29回演習課題

--- src/test/java/raisetech/Student/Management/controller/converter/StudentConverterTest.java
+++ src/test/java/raisetech/Student/Management/controller/converter/StudentConverterTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import raisetech.student.management.controller.StudentController;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
+import raisetech.student.management.domain.StudentDetail;
 
 @WebMvcTest(StudentConverter.class)
 class StudentConverterTest {
@@ -15,6 +16,11 @@ class StudentConverterTest {
   void 受講生と受講コース情報が紐づけられ受講生詳細となっていること(){
     List<Student> studentList = new ArrayList<>();
     List<StudentCourse> studentCourseList = new ArrayList<>();
+
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudent(student);
+    studentDetail.setStudentCourseList(List.of(course));
+
 
   }
 
