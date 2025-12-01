9a2cfbaf301240af99150c2dbc0ad3ff89c36e6c
ruzo-814 <unagi.0418.water@gmail.com>
2025-12-01 15:00:27 +0900

    Repositoryのテストクラスを作成しました

---
+++ src/test/java/raisetech/Student/Management/repository/StudentRepositoryTest.java
@@ -0,0 +1,11 @@
+package raisetech.student.management.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+
+@MybatisTest
+
+class StudentRepositoryTest {
+
+}
